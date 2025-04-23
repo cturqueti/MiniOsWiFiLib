827a4b3f72d578706bd0b5ffa45a3d065ac68eef
Carlos Augusto D'Orazio Turqueti <cturqueti@yahoo.com.br>
2025-04-23 14:23:40 -0300

    Remove some itens not used

--- Examples/Example.cpp
+++ Examples/Example.cpp
@@ -1,14 +1,12 @@
 
-#define VERSAO "1.0"
-#include "ErrorLib.h"
+#define VERSAO "2.0"
 #include "WiFiLib.h"
 #include <Arduino.h>
+#include <ErrorLib.h>
 #include <LittleFS.h>
 #include <LogLibrary.h>
 #include <SPI.h>
 #include <WiFiItems.h>
-// #include "ServicesManager.h"
-// #include "Utils.h"
 
 // Definição dos pinos para a serial alternativa (RX, TX)
 #define UART_RX_PIN 44
@@ -20,8 +18,6 @@
 // Instância da serial alternativa
 HardwareSerial HardwareSerialPort(0); // UART0
 
-// WiFiCredentialsJSON json;
-
 void setup()
 {
     HardwareSerialPort.begin(MAIN_SERIAL_BAUDRATE, SERIAL_8N1, UART_RX_PIN, UART_TX_PIN);
@@ -30,9 +26,6 @@ void setup()
 
     vTaskDelay(pdMS_TO_TICKS(2000));
     LOG_INFO("[MAIN] Iniciando MiniOS v%s", VERSAO);
-
-    // auto& littleFs = LittleFSLib::getInstance(true);
-    // auto& nvs = WiFiCredentialsNVS::getInstance(WiFiLog::ENABLE);
 
     auto &wifiCommon = WiFiLib::getInstance(WiFiLog::ENABLE);
     if (wifiCommon.isConfigLoaded())
